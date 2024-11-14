815a6168d15b3314174b0790e89089fcb930810a
Frank Navarro <fnavarro7777@gmail.com>
2024-11-14 18:27:45 +0000

    feat: You can't get rid of a classic.
    fix: Make classic available again.
    
    chore: Classic chores...

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,9 +4,15 @@ namespace MyPHP\Fun;
 
 final class HelloWorld
 {
+    private const CLASSIC = "Hello world!";
     private const OLD_BEN = "Hello there...";
     private const SPOCK = "Live long and prosper.";
     private const DROID = "Roger! Roger!";
+
+    public function greet()
+    {
+        return self::CLASSIC;
+    }
 
     public function greetSW()
     {
