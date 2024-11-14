06837eb5920e8ffcdeac51c915faf23a12f4dc82
Frank Navarro <fnavarro7777@gmail.com>
2024-11-14 18:25:30 +0000

    fix: Fix the naming of Old Ben.

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,13 +4,13 @@ namespace MyPHP\Fun;
 
 final class HelloWorld
 {
-    private const OBI = "Hello there...";
+    private const OLD_BEN = "Hello there...";
     private const SPOCK = "Live long and prosper.";
     private const DROID = "Roger! Roger!";
 
     public function greetSW()
     {
-        return self::OBI;
+        return self::OLD_BEN;
     }
 
     public function greetST()
